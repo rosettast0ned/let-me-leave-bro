2233a103b6f8bc696a4f43daa55ca2e478a9c246
Christopher Conley <christopher-conley@users.noreply.github.com>
2024-12-30 08:36:40 -0500

    It works, send it

--- tampermonkey/let-me-leave-bro.user.js
+++ tampermonkey/let-me-leave-bro.user.js
@@ -3,29 +3,44 @@
 // @author       Christopher Conley
 // @copyright    Copyright (C) 2024 Christopher Conley
 // @license      MIT
-// @version      0.2.2
+// @version      2024.12.30
 // @description  Do you hate "Are you SURE you want to leave this page?" dialogs that pop up when you try to close a tab? So do I. This is a Chrome extension and Tampermonkey script that prevents execution of those Javascript dialogs that prevent you from leaving a page.
 // @namespace    https://github.com/rosettast0ned/let-me-leave-bro
 // @source       https://github.com/rosettast0ned/let-me-leave-bro
 // @supportURL   https://github.com/rosettast0ned/let-me-leave-bro/issues
 // @match        *://*/*
-// @icon         https://raw.githubusercontent.com/rosettast0ned/let-me-leave-bro/main/tampermonkey/let_me_leave_bro.png
-// @icon64       https://raw.githubusercontent.com/rosettast0ned/let-me-leave-bro/main/tampermonkey/let_me_leave_bro64.png
-// @run-at       document-idle
+// @icon         https://raw.githubusercontent.com/rosettast0ned/let-me-leave-bro/refs/heads/main/tampermonkey/let_me_leave_bro.png
+// @icon64       https://raw.githubusercontent.com/rosettast0ned/let-me-leave-bro/refs/heads/main/tampermonkey/let_me_leave_bro64.png
+// @run-at       document-start
 // ==/UserScript==
 //
 //
 
 (function () {
-    console.log('LMLB: Let Me Leave, Bro extension triggered.')
+    console.log('LMLB: Let Me Leave, Bro extension loaded.')
 
-    window.addEventListener("beforeunload", event => event.stopPropagation(), { capture: true });
-    window.addEventListener("onbeforeunload", event => event.stopPropagation(), { capture: true });
-    window.addEventListener("unload", event => event.stopPropagation(), { capture: true });
-    window.addEventListener("onunload", event => event.stopPropagation(), { capture: true });
-    document.addEventListener("beforeunload", event => event.stopPropagation(), { capture: true });
-    document.addEventListener("onbeforeunload", event => event.stopPropagation(), { capture: true });
-    document.addEventListener("onunload", event => event.stopPropagation(), { capture: true });
-    document.addEventListener("unload", event => event.stopPropagation(), { capture: true });
+    const StopIt = (event) => {
+        console.log('LMLB: Intercepting unload events.')
+        event.stopImmediatePropagation();
+        event.stopPropagation();
+    };
+
+    const AddListeners = () => {
+        window.addEventListener("beforeunload", StopIt, { capture: true });
+        document.addEventListener("beforeunload", StopIt, { capture: true });
+        window.addEventListener("unload", StopIt, { capture: true });
+        document.addEventListener("unload", StopIt, { capture: true });
+        console.log('LMLB: Added listeners.');
+    };
+
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', () => {
+            console.log('LMLB: Document loaded, adding listeners.');
+            AddListeners();
+        });
+    } else {
+        console.log('LMLB: Document already loaded, adding listeners.');
+        AddListeners();
+    }
 
 })();
