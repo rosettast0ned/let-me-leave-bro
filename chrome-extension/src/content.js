6902b2aec6da637debb7a54a537d53408e026e27
Christopher Conley <christopher-conley@users.noreply.github.com>
2024-12-30 06:56:37 -0500

    Works fine in tampermonkey, but of course Chrome has to be an asshole

--- chrome-extension/src/content.js
+++ chrome-extension/src/content.js
@@ -6,13 +6,29 @@
 // @description  Do you hate "Are you SURE you want to leave this page?" dialogs that pop up when you try to close a tab? So do I. This is a Chrome extension and Tampermonkey script that prevents execution of those Javascript dialogs that prevent you from leaving a page.
 //
 
-console.log('LMLB: Let Me Leave, Bro extension triggered.')
+console.log('LMLB: Let Me Leave, Bro extension loaded.')
 
-window.addEventListener("beforeunload", event => event.stopPropagation(), { capture: true });
-window.addEventListener("onbeforeunload", event => event.stopPropagation(), { capture: true });
-window.addEventListener("unload", event => event.stopPropagation(), { capture: true });
-window.addEventListener("onunload", event => event.stopPropagation(), { capture: true });
-document.addEventListener("beforeunload", event => event.stopPropagation(), { capture: true });
-document.addEventListener("onbeforeunload", event => event.stopPropagation(), { capture: true });
-document.addEventListener("onunload", event => event.stopPropagation(), { capture: true });
-document.addEventListener("unload", event => event.stopPropagation(), { capture: true });
+function StopIt(event) {
+    console.log('LMLB: Intercepting unload events.')
+    event.stopPropagation();
+}
+
+window.addEventListener("beforeunload", StopIt, { capture: true });
+window.addEventListener("onbeforeunload", StopIt, { capture: true });
+window.addEventListener("unload", StopIt, { capture: true });
+window.addEventListener("onunload", StopIt, { capture: true });
+document.addEventListener("beforeunload", StopIt, { capture: true });
+document.addEventListener("onbeforeunload", StopIt, { capture: true });
+document.addEventListener("onunload", StopIt, { capture: true });
+document.addEventListener("unload", StopIt, { capture: true });
+
+
+
+// window.addEventListener("beforeunload", event => event.stopPropagation(), { capture: true });
+// window.addEventListener("onbeforeunload", event => event.stopPropagation(), { capture: true });
+// window.addEventListener("unload", event => event.stopPropagation(), { capture: true });
+// window.addEventListener("onunload", event => event.stopPropagation(), { capture: true });
+// document.addEventListener("beforeunload", event => event.stopPropagation(), { capture: true });
+// document.addEventListener("onbeforeunload", event => event.stopPropagation(), { capture: true });
+// document.addEventListener("onunload", event => event.stopPropagation(), { capture: true });
+// document.addEventListener("unload", event => event.stopPropagation(), { capture: true });
